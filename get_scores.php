22ce89657fd9c54253ab7a5f32c72f8bba2f5e4a
AndreaP131 <andrea.pradas.andrea@gmail.com>
2025-10-07 18:31:49 +0200

    Trabajo básico en save_score y get_scores

--- get_scores.php
+++ get_scores.php
@@ -2,10 +2,15 @@
 // ============== GUÍA PARA EL BACK-END ============== (get_scores.php)  
 // PASO 1: Configurar la cabecera de la respuesta.  
 // header('Content-Type: ...');  
-// PASO 2: Definir la ruta del archivo de puntuaciones. // $scoresFile = 'scores.json';  
+header ('Content-Type: application/json');
+// PASO 2: Definir la ruta del archivo de puntuaciones. // 
+$scoresFile = 'scores.json';  
 // PASO 3: Leer las puntuaciones existentes.  
-// $scores = [];  
-// Si el archivo $scoresFile existe, léelo y decodifícalo a PHP. // if (file_exists($scoresFile)) { ... }  
+$scores = [];  
+// Si el archivo $scoresFile existe, léelo y decodifícalo a PHP. // 
+if (file_exists($scoresFile)) { 
+
+ }  
 // PASO 4: Enviar las puntuaciones como JSON.  
-// echo json_encode($scores);  
+echo json_encode($scores);  
 ?> 
